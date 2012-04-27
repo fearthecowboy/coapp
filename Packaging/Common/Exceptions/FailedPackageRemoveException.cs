17acb95ace791c5ab513b1e8edc5a6809264c2a8
Garrett Serack <garretts@microsoft.com>
2012-04-26 21:59:44 -0700

    refactoring

---
+++ Packaging/Common/Exceptions/FailedPackageRemoveException.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="CoApp Project">
+//     Copyright (c) 2010-2012 Garrett Serack and CoApp Contributors. 
+//     Contributors can be discovered using the 'git log' command.
+//     All rights reserved.
+// </copyright>
+// <license>
+//     The software is licensed under the Apache 2.0 License (the "License")
+//     You may not use the software except in compliance with the License. 
+// </license>
+//-----------------------------------------------------------------------
+
+namespace CoApp.Packaging.Common.Exceptions {
+    using Toolkit.Exceptions;
+
+    public class FailedPackageRemoveException : CoAppException {
+        public string CanonicalName { get; internal set; }
+        public string Reason { get; internal set; }
+        public FailedPackageRemoveException (string canonicalName, string reason, params object[] args) {
+            CanonicalName = canonicalName;
+            Reason = string.Format(reason, args);
+        }
+    }
+}
